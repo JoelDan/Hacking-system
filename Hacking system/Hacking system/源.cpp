a95b7cc1fd16f4d690ea5f85a9a389572ef8fbba
JoelDan <zdl0623@outlook.com>
2019-05-08 09:27:48 +0800

    实现判断账号和密码是否正确

--- Hacking system/Hacking system/源.cpp
+++ Hacking system/Hacking system/源.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include <Windows.h>
+#include <string>
+
+using namespace std;
 
 int main(void) {
-	std::cout << "hello world" << std::endl;
+	string name;
+	string qwd;
+
+	cout << "ÇëÊäÈëÓÃ»§Ãû£º";
+	cin >> name;
+
+	cout << "ÇëÊäÈëÃÜÂë£º";
+	cin >> qwd;
+	system("cls");
+
+	if (name == "54hk" && qwd == "123456") {
+		cout << "1.ÍøÕ¾404¹¥»÷" << endl;
+		cout << "2.ÍøÕ¾´Û¸Ä¹¥»÷" << endl;
+		cout << "3.ÍøÕ¾¹¥»÷¼ÇÂ¼" << endl;
+		cout << "4.DNS¹¥»÷" << endl;
+		cout << "5.·þÎñÆ÷ÖØÆô¹¥»÷" << endl;
+	}
+	else {
+		cout << "ÓÃ»§Ãû»òÃÜÂë´íÎó£¬ÇëÖØÐÂÊäÈë£¡" << endl;
+	}
 
 	system("pause");
 	return 0;
